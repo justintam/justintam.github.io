aa501f5924a606d840b63083cbc6e144d96dbf60
Justin Tam <23109190+justintam@users.noreply.github.com>
2024-03-26 23:46:59 -0500

    Update basic-tree.js

--- family/view/basic-tree.js
+++ family/view/basic-tree.js
@@ -14,11 +14,11 @@ fetch("./data.json").then(r => r.json()).then(data => {
       store,
       svg: view.svg,
       card_dim: {w:220,h:70,text_x:75,text_y:15,img_w:60,img_h:60,img_x:5,img_y:5},
-      card_display: [d => d.data.label || '', d => d.data.desc || ''],
+      card_display: [d => d.data.label || '', d => d.data.birth || '?' + ' - ' + d => d.data.death || '?', d => d.data.desc || ''],
       mini_tree: true,
       link_break: false
     })
-
+  
   view.setCard(Card)
   store.setOnUpdate(props => view.update(props || {}))
   store.update.tree({initial: true})
