9683774fc13502577bae097fa5dfbe825b3c4dfb
Justin Tam <23109190+justintam@users.noreply.github.com>
2024-03-27 00:01:56 -0500

    Update basic-tree.js

--- family/view/basic-tree.js
+++ family/view/basic-tree.js
@@ -14,7 +14,7 @@ fetch("./data.json").then(r => r.json()).then(data => {
       store,
       svg: view.svg,
       card_dim: {w:220,h:70,text_x:75,text_y:15,img_w:60,img_h:60,img_x:5,img_y:5},
-      card_display: [d => d.data.label || '', d => '(' + (d.data.birth || '?') + ' - ' + (d.data.death || '?') + ')', d => d.data.desc || ''],
+      card_display: [d => d.data.label || '', d => (d.data.fn || '') + ' ' + (d.data.ln || ''), d => '(' + (d.data.birth || '?') + ' - ' + (d.data.death || '?') + ')', d => d.data.desc || ''],
       mini_tree: true,
       link_break: false
     })
