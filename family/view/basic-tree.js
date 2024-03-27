2d167b8ebdd0836b4c573f075755e5aa41515c62
Justin Tam <23109190+justintam@users.noreply.github.com>
2024-03-27 00:04:26 -0500

    Update basic-tree.js

--- family/view/basic-tree.js
+++ family/view/basic-tree.js
@@ -13,8 +13,8 @@ fetch("./data.json").then(r => r.json()).then(data => {
     Card = f3.elements.Card({
       store,
       svg: view.svg,
-      card_dim: {w:220,h:70,text_x:75,text_y:15,img_w:60,img_h:60,img_x:5,img_y:5},
-      card_display: [d => d.data.label || '', d => (d.data.fn || '') + ' ' + (d.data.ln || ''), d => '(' + (d.data.birth || '?') + ' - ' + (d.data.death || '?') + ')', d => d.data.desc || ''],
+      card_dim: {w:220,h:300,text_x:75,text_y:15,img_w:60,img_h:60,img_x:5,img_y:5},
+      card_display: [d => d.data.label || '', d => (d.data.ln || '') + ' ' + (d.data.fn || ''), d => '(' + (d.data.birth || '?') + ' - ' + (d.data.death || '?') + ')', d => d.data.desc || ''],
       mini_tree: true,
       link_break: false
     })
