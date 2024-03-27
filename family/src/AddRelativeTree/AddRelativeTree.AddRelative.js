d40ed6d507d3b7b5c51db9fe4cf152040feb84c9
Justin Tam <23109190+justintam@users.noreply.github.com>
2024-03-26 23:20:16 -0500

    Update AddRelativeTree.AddRelative.js

--- family/src/AddRelativeTree/AddRelativeTree.AddRelative.js
+++ family/src/AddRelativeTree/AddRelativeTree.AddRelative.js
@@ -3,9 +3,9 @@ import AddRelativeTree from "./AddRelativeTree.js"
 
 export function AddRelative({store, cont, card_dim, cardEditForm, labels}) {
   return function ({d, scale}) {
-    const transition_time = 1000
+    const transition_time = 0
 
-    if (!scale && window.innerWidth < 650) scale = window.innerWidth / 650
+    if (!scale && window.innerWidth < 800) scale = window.innerWidth / 800
     toggleAllRels(store.getTree().data, false)
     store.update.mainId(d.data.id);
     store.update.tree({tree_position: 'main_to_middle', transition_time, scale})
